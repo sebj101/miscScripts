ede56d951ab85004c6ac522fd8cd6ad5954b436b
sebj101 <jonesseb100@gmail.com>
2019-10-16 13:20:59 +0100

    Started bar source script

---
+++ hptpctof/barSourceTests.C
@@ -0,0 +1,8 @@
+// barSourceTests.C
+// Uses data taken at UCL with a strontium-90 source
+
+void barSourceTests(const char* saveDir, const char* fileDir="/unix/dune/tof/") 
+{
+
+
+} // barSourceTests
